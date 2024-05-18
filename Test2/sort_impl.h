6c27188fcc58c5a1517be7f8f5e2975bdb4ab4b6
Appmedia06 <joey5489joey@gmail.com>
2024-05-18 21:55:45 +0800

    Complete timsort algorithm

--- Test2/sort_impl.h
+++ Test2/sort_impl.h
@@ -1,3 +1,9 @@
+#ifndef SORT_IMPL_H
+#define SORT_IMPL_H
+
+#include "list.h"
+// #include "timsort.c"
+
 struct list_head;
 
 typedef int (*list_cmp_func_t)(void *,
@@ -5,3 +11,5 @@ typedef int (*list_cmp_func_t)(void *,
                                const struct list_head *);
 
 void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+
+#endif
